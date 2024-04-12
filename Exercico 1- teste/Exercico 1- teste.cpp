0f4bd783e8dfc040fac155eeeee7fb7449300d89
Laura <133364744+laurajhagsuUGAVGHA@users.noreply.github.com>
2024-04-12 15:07:10 -0300

    .

---
+++ Exercico 1- teste/Exercico 1- teste.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+//PRIMEIRO EXERCICIO
+/*using namespace std;
+
+int main() {
+	int instalacao = 300;
+	int limpeza = 150;
+	double manutencao = 499.99;
+
+	float soma = instalacao + limpeza + manutencao;
+	cout << soma;
+
+	return 0;
+}
+
+*/
+
+//SEGUNDO EXERCICIO
+
+/*using namespace std;
+
+int main() {
+	int valor = 35;
+	int hora = 8;
+
+	int mult = valor * hora;
+	cout << mult;
+}
+*/
+
+//TERCEIRO EXERCICIO 
+
+/*using namespace std;
+
+int main() {
+	int bombom = 27;
+	int pamela = 16;
+
+	int mult = bombom * pamela;
+	cout << mult;
+}
+*/ 
+
+//QUARTO EXERCICO 
+
+/*using namespace std;
+
+int main() {
+	int loja = 300;
+	int caixa = 20;
+
+	int div = loja / caixa;
+	cout << div;
+}
+*/
+
+//QUINTO EXERCICIO 
+
+using namespace std;
+
+int main() {
+	int comprou = 120;
+	int vendeu = 80;
+	int custo = 30;
+	int comproou = 50;
+	int restou = 40;
+
+	int mult = vendeu * custo;
+	int soma =  comproou + restou ;
+		cout << sub;
+		cout << "\n";
+	    cout << mult;
+	    cout << "\n";
+	    cout << soma;
+
+}
